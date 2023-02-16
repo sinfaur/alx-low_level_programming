f4ba19acc55d982d33102a89e6f6cdadfecd821a
Yassine <yassine.bennani@aiesec.net>
2023-02-16 12:11:29 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- * main -Print a line of code using puts
- *
- * Return: Always 0
- */
-int main (void)
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+			return (0);
 }
+
